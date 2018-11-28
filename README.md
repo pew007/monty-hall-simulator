6f35a2c715066398cc4d5be599fcd306ea707764
Pengyu Wang <bird0407@gmail.com>
2018-11-27 22:37:00 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on November 28, 2018, 6:03 am.
---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig');
+    }
+}

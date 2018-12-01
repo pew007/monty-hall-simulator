2d1dd7e6ea27f9f7b90daf25acc1c9eb6637e236
Pengyu Wang <bird0407@gmail.com>
2018-11-30 18:43:24 -0800

    add controller test

--- tests/AppBundle/Controller/AppControllerTest.php
+++ tests/AppBundle/Controller/AppControllerTest.php
@@ -12,7 +12,40 @@ class AppControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Monty Hall Problem Simulator', $crawler->filter('.container h1')->text());
+        $this->assertEquals(
+            200,
+            $client->getResponse()
+                   ->getStatusCode()
+        );
+        $this->assertContains(
+            'Monty Hall Problem Simulator',
+            $crawler->filter('.container h1')
+                    ->text()
+        );
+    }
+
+    public function testSimulate()
+    {
+        $client  = static::createClient();
+        $crawler = $client->request('GET', '/simulate/100');
+
+        $this->assertEquals(
+            200,
+            $client->getResponse()
+                   ->getStatusCode()
+        );
+        $this->assertNotEmpty($crawler->filter('.table'));
+    }
+
+    public function testSimulateBadInput()
+    {
+        $client  = static::createClient();
+        $crawler = $client->request('GET', '/simulate/badinput');
+
+        $this->assertEquals(
+            404,
+            $client->getResponse()
+                   ->getStatusCode()
+        );
     }
 }
